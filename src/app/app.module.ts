76392a8b8c13dc6a1a6e294c817426b8800b2146
Carlos Calzadilla <maleras24@gmail.com>
2025-11-11 16:18:36 +0000

    Fin de seccion 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,19 @@ import { App } from './app';
 
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
     App,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CounterModule,
     HeroesModule,
+    DbzModule
 ],
   providers: [
     provideBrowserGlobalErrorListeners()
